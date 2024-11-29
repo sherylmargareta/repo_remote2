07f483b6ca0bac82b7546af0c93cb651419652fa
sherylmargareta <sherylmargareta28@gmail.com>
2024-11-29 10:39:01 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Menambahkan Local Remote</title>
+</head>
+<body>
+	<h1><?php echo "Lorem Ipsum"; ?></h1>
+	<p><?php echo "Dolor Sit Amet"; ?></p>
+</body>
+</html>
